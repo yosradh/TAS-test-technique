e8c3e30f311660be05c81d3b83e96c0665897c53
yosradh <yosra.dhibi.93@gmail.com>
2022-08-07 01:41:26 +0200

    init commit

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+using System;
+
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.generation = new System.Windows.Forms.Button();
+            this.pathFichier = new System.Windows.Forms.TextBox();
+            this.parcourir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.generation);
+            this.groupBox1.Controls.Add(this.pathFichier);
+            this.groupBox1.Controls.Add(this.parcourir);
+            this.groupBox1.Location = new System.Drawing.Point(107, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 181);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Importation des données";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // generation
+            // 
+            this.generation.Location = new System.Drawing.Point(130, 128);
+            this.generation.Name = "generation";
+            this.generation.Size = new System.Drawing.Size(110, 23);
+            this.generation.TabIndex = 2;
+            this.generation.Text = "Génération";
+            this.generation.UseVisualStyleBackColor = true;
+            this.generation.Click += new System.EventHandler(this.generation_Click);
+            // 
+            // pathFichier
+            // 
+            this.pathFichier.Location = new System.Drawing.Point(21, 75);
+            this.pathFichier.Name = "pathFichier";
+            this.pathFichier.Size = new System.Drawing.Size(328, 20);
+            this.pathFichier.TabIndex = 1;
+            // 
+            // parcourir
+            // 
+            this.parcourir.Location = new System.Drawing.Point(21, 33);
+            this.parcourir.Name = "parcourir";
+            this.parcourir.Size = new System.Drawing.Size(92, 23);
+            this.parcourir.TabIndex = 0;
+            this.parcourir.Text = "Parcourir ...";
+            this.parcourir.UseVisualStyleBackColor = true;
+            this.parcourir.Click += new System.EventHandler(this.parcourir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 663);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button generation;
+        private System.Windows.Forms.TextBox pathFichier;
+        private System.Windows.Forms.Button parcourir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,240 @@
+﻿using ExcelDataReader;
+using IronPdf;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace Test
+{
+    public class Employee
+    {
+        public string Name;
+        public string Surname;
+        public float TotalSalary
+        {
+            get { return M1 + M2 + M3; }
+        }
+
+        public float M1;
+        public float M2;
+        public float M3;    
+
+        public string GetEmployeeText(int index)
+        {
+            string employeeString = string.Format("{0:000}", index);
+
+            string EmployeeName = this.Name + "" + this.Surname;
+
+            employeeString += string.Format("{0,-30}", EmployeeName);
+
+            employeeString += string.Format("{0:0000000000}", this.TotalSalary);
+
+            return employeeString;
+        }
+
+        //public string [] GetEmployeePDF()
+        //{   
+            
+        //    string employeeString = string.Format("{0:000}", index);
+
+        //    string EmployeeName = this.Name + "" + this.Surname;
+
+        //    employeeString += string.Format("{0,-30}", EmployeeName);
+
+        //    employeeString += string.Format("{0:0000000000}", this.TotalSalary);
+
+        //    return employeeString;
+        //}
+    }
+
+    public partial class Form1 : Form
+    {
+
+        string myPath = @"Cnss.txt";
+
+
+        public List<Employee> employees = new List<Employee>();
+
+
+        public void CreateEmployees(DataTable table)
+        {
+            employees.Clear();
+
+            Employee FirstEmployee = new Employee();
+
+            FirstEmployee.Name = table.Columns[0].ColumnName;
+            FirstEmployee.Surname = table.Columns[1].ColumnName;
+
+            FirstEmployee.M1 = float.Parse(table.Columns[2].ColumnName) * 1000;
+            FirstEmployee.M2 = float.Parse(table.Columns[3].ColumnName) * 1000;
+            FirstEmployee.M3 = float.Parse(table.Columns[4].ColumnName) * 1000;
+
+
+            employees.Add(FirstEmployee);
+
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                Employee employee = new Employee();
+
+                employee.Name = table.Rows[i][table.Columns[0]].ToString();
+                employee.Surname = table.Rows[i][table.Columns[1]].ToString();
+
+                employee.M1 = float.Parse((table.Rows[i][table.Columns[2]].ToString())) * 1000;
+                employee.M2 = float.Parse((table.Rows[i][table.Columns[3]].ToString())) * 1000;
+                employee.M3 = float.Parse((table.Rows[i][table.Columns[4]].ToString())) * 1000;
+
+
+
+                employees.Add(employee);
+            }
+        }
+
+        public string Format(DataTable table)
+        {
+
+            #region old traitment
+            //string FileText = string.Empty;
+
+            //int index = 1;
+
+            //FileText += string.Format("{0:000}", index);
+            //FileText += "" + '\t';
+
+            //DataRow row1 = table.NewRow();
+
+            //foreach (DataColumn col in table.Columns)
+            //{
+
+            //    FileText += string.Format("{0,-14}", col.ColumnName + "" + '\t');
+
+            //    //Console.Write("{0,-14}", col.ColumnName);
+            //}
+            ////Console.WriteLine();
+
+            //FileText += "" + '\n';
+
+
+
+            //for (int i = 0; i < table.Rows.Count; i++)
+            //{
+            //    index++;
+
+            //    FileText += string.Format("{0:000}", index);
+            //    FileText += "" + '\t';
+
+            //    for (int j = 0; j < table.Columns.Count; j++)
+            //    {
+            //        if (table.Columns[j].DataType.Equals(typeof(DateTime)))
+            //        {
+            //            // Console.Write("{0,-14:d}", table.Rows[i][table.Columns[j]]);
+            //        }
+
+            //        else if (table.Columns[j].DataType.Equals(typeof(Decimal)))
+            //        {
+            //            //Console.Write("{0,-14:C}", table.Rows[i][table.Columns[j]]);
+            //        }
+
+            //        else
+            //            // Console.Write("{0,-14}",table.Rows[i][table.Columns[j]]);
+
+
+            //            FileText += string.Format("{0,-14}", table.Rows[i][table.Columns[j]] + "" + '\t');
+            //    }
+            //    // Console.WriteLine();
+            //    FileText += "" + '\n';
+            //}
+
+            //Console.WriteLine(FileText);
+
+            //return FileText;
+
+
+            ////foreach (DataRow row in table.Rows)
+            ////{
+            ////    foreach (DataColumn col in table.Columns)
+            ////    {
+
+            ////    }
+            ////    Console.WriteLine();
+            ////}
+            ////Console.WriteLine();
+            #endregion
+
+            CreateEmployees(table);
+
+            string FileText = string.Empty;
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                FileText += employees[i].GetEmployeeText(i + 1) + '\n';
+            }
+            Console.WriteLine(FileText);
+
+            return FileText;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void parcourir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Choisir votre fichier ",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "xls",
+                Filter = "xls files (*.xls)|*.xls",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pathFichier.Text = openFileDialog.FileName.ToString();
+            }
+        }
+
+
+        private void generation_Click(object sender, EventArgs e)
+        {
+
+            using (var stream = File.Open(pathFichier.Text, FileMode.Open, FileAccess.Read))
+            {
+                using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    DataSet resultat = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                    });
+
+                    File.WriteAllText(myPath, Format(resultat.Tables[0]));
+                }
+            }
+
+
+
+            var Renderer = new ChromePdfRenderer();
+            Renderer.RenderHtmlAsPdf("<h1>This is the Tutorial for C# Generate PDF<h1>").SaveAs("GeneratePDF.pdf");
+
+        }
+
+
+    }
+}
